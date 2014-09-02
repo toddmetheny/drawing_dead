9431bc286786af6f05da6b01550489927d7275d1
Todd Metheny <todd.metheny@gmail.com>
2014-09-02 13:12:50 -0400

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the content 'Drawing Dead'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      expect(page).to have_content('Drawing Dead')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Drawing Dead | Home")
+    end  
+  end
+
+  describe "Help page" do
+  	it "should have the content 'Help'" do
+  		visit '/static_pages/help'
+  		expect(page).to have_content('Help')
+  	end
+    
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Drawing Dead | Help")
+    end  
+  end
+
+  describe "About page" do
+  	it "should have the content 'About Us'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('About Us')
+  	end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Drawing Dead | About Us")
+    end
+  end
+end
