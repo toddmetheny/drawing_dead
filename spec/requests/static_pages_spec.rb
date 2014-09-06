7a59c3cb7035c8f9f78d9bed46bbe53e60ba0bfc
Todd Metheny <todd.metheny@gmail.com>
2014-09-06 16:39:03 -0400

    Exercises from ch 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,31 +6,51 @@ describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Drawing Dead" }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
-    it { should have_content ('Drawing Dead')}
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-    it { should have_title("#{base_title}") }
+    let(:heading) { 'Drawing Dead' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title ('| Home') } 
   end
 
   describe "Help page" do
     before { visit help_path }
-  	it { should have_content('Help') }
-    it { should have_title(full_title('Help')) } 
+  	
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' } 
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About Us' }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    #click_link "Home"
+    #click_link "Sign up now!"
+    #expect(page).to # fill in
+    #click_link "drawing dead"
+    #expect(page).to #fill in
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,5 @@
+include ApplicationHelper
+
 def full_title(page_title)
 	base_title = "Ruby on Rails Tutorial Drawing Dead"
 	if page_title.empty?
