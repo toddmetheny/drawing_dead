95513d529785632b39b389be3d67ba7f938b4517
Todd Metheny <todd.metheny@gmail.com>
2014-09-19 15:40:22 -0400

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)	# not the final implementation
   	if @user.save
-      flash[:success] = "Welcome to the Drawing Dead App"
+      sign_in @user
+      flash[:success] = "Welcome to the Drawing Dead App!"
   		redirect_to @user
   	else
   		render 'new'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 describe User do
   before do
-  	@user = User.new(name: "Example User", email: "user@example.com",
-                      password: "foobar", password_confirmation: "foobar")
+  	@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
 
@@ -14,6 +13,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -108,6 +108,16 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
+
+
+
+
+
 
 
