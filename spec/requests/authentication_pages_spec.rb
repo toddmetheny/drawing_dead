559ed1ca4f54ca34565dcbb79b30cef9a910ac3b
Todd Metheny <todd.metheny@gmail.com>
2014-09-26 15:17:06 -0400

    Exercises at the end of Ch 9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,6 +14,9 @@ describe "Authentication" do
   describe "signin" do
   	before { visit signin_path }
 
+    # it { should_not have_link('Profile', href: user_path(user)) }
+    # it { should_not have_link('Settings', href: edit_user_path(user)) }
+
   	describe "with invalid information" do
   		before { click_button "Sign in" }
 
@@ -31,7 +34,7 @@ describe "Authentication" do
       before { sign_in user }
 
       it { should have_title(user.name) }
-      it { should have_link('Users', href: users_path) }
+      #it { should have_link('Users', href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
@@ -61,6 +64,20 @@ describe "Authentication" do
 
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              click_link "Sign out"
+              visit signin_path
+              fill_in "Email", with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
